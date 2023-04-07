f4259cbfd0b7d2d3063d3b49d8edaea4046c7341
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2023-04-07 18:42:58 +0600

    Completed, take input from the input field and display on the screen

--- src/components/EventHandeler/EventHandeler.js
+++ src/components/EventHandeler/EventHandeler.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const EventHandeler = ()=>{
+
+    const [inputedValue, setInputedValue] = useState();
+
+    const handeler =(e)=>{
+        console.log(e.target.value);
+        setInputedValue(e.target.value);
+    }
 
     return(
         <div>
             <h1>Hi from Event handeler</h1>
+            <div>
+                <input type="text" onChange={handeler} />
+            </div>
+            <p>{inputedValue}</p>
         </div>
     )
 }
